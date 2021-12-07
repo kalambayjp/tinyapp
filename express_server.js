8e4b2e47e0f57d8caca91a621f32f184b02cdbd6
JP <kalambayjp@gmail.com>
2021-12-06 22:08:31 -0800

    added urls_show.ejs, renders single url and shortened url to user

--- express_server.js
+++ express_server.js
@@ -9,9 +9,14 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls", (req, res) => {
+app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars)
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  res.render('urls_show', templateVars)
 })
 
 app.get('/', (req, res) => { 
