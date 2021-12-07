3d493bb057b1f37cde1cd375db29d5268bc9630d
JP <kalambayjp@gmail.com>
2021-12-07 14:52:22 -0800

    added delete functionality, and some comments for simplicity

--- express_server.js
+++ express_server.js
@@ -19,44 +19,80 @@ function generateRandomString() {
   }
   return randomString;
 };
-
+//route to home page with list of URLs
 app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars)
 });
+        // create post request to delete URL
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
+})
 
+
+// route to create new URL page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-
+        // post to create new short/long URL pair
 app.post("/urls", (req, res) => {
-  console.log(req.body); 
+   
   const shortURL = generateRandomString()
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);        
 });
 
+
+// create page with specific short and long URL data
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  res.render('urls_show', templateVars)
+})
+       // ceate link to full length url page from shor URL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
-app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
-  res.render('urls_show', templateVars)
-})
 
-app.get('/', (req, res) => { 
-  res.send('Hellooo');
-});
 
-app.get('/urls.json', (req, res) => { 
-  res.json(urlDatabase);
-});
 
-app.get('/hello', (req, res) => { 
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
-})
 app.listen(port, () => {
- console.log(`Example app listening on port ${port}!`);
+  console.log(`Example app listening on port ${port}!`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// examples code:
+// app.get('/', (req, res) => { 
+//   res.send('Hellooo');
+// });
+
+// app.get('/urls.json', (req, res) => { 
+//   res.json(urlDatabase);
+// });
+
+// app.get('/hello', (req, res) => { 
+//   res.send("<html><body>Hello <b>World</b></body></html>\n")
+// })
