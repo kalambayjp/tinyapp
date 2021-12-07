948429380efe4bb326e38169898ff50e200752ee
JP <kalambayjp@gmail.com>
2021-12-06 21:37:12 -0800

    added route handler to urls object to urls_index.ejs file, rendered datd to client side

--- express_server.js
+++ express_server.js
@@ -2,23 +2,29 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get('/', (req, res) => {
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars)
+})
+
+app.get('/', (req, res) => { 
   res.send('Hellooo');
 });
 
-app.listen(port, () => {
- console.log(`Example app listening on port ${port}!`);
-});
-
-app.get('/urls.json', (req, res) => {
+app.get('/urls.json', (req, res) => { 
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
+app.get('/hello', (req, res) => { 
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 })
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}!`);
+});
